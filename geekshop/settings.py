edf9e1c7a8c89cf24b593004aaf55e674074856f
KazuzGit <zazuk2002@mail.ru>
2022-01-15 19:07:48 +0300

    Lesson 4. Code style fix

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -21,7 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-9i#7_#%$+&a9^118ao5s8x7bqtd^7v)j$4&116oyrhn4u#2s&7"
+SECRET_KEY = (
+    "django-insecure-9i#7_#%$+&a9^118ao5s8x7bqtd^7v)j$4&116oyrhn4u#2s&7"
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
